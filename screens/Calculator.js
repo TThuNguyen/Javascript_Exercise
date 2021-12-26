2ca073e0b0a1357e6579019c56e8cd503a8294b1
TThuNguyen <86250805+TThuNguyen@users.noreply.github.com>
2021-12-26 18:10:53 +0700

    Update Calculator.js

--- screens/Calculator.js
+++ screens/Calculator.js
@@ -1,11 +1,8 @@
 import React from "react";
+import { useState } from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default function CalculatorScreen() {
-  const buttonClickedHandler = () => {
-    console.log("You have been clicked a button!");
-    // do something
-  };
   const leftItems = [
     "/",
     "±",
@@ -22,46 +19,58 @@ export default function CalculatorScreen() {
     ".",
     "0",
   ];
+  const [isResult, setIsResult] = useState("");
   const rightItems = ["x", "-", "+"];
+  const [isPress, setIsPress] = useState("");
+  const handleClick = (item) => {
+    if (item === "C") {
+      setIsPress("");
+      setIsResult("");
+    } else {
+      setIsPress(`${isPress}${item}`);
+    }
+  };
+
+  const handleClickEq = () => {
+    setIsResult(eval(isPress.replace(/x/g, "*")));
+  };
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={{ fontSize: 20 }}>{5 * 5}</Text>
-        <Text style={{ fontSize: 50 }}>25</Text>
+        <Text style={{ fontSize: 20 }}>{isPress}</Text>
+        <Text style={{ fontSize: 50 }}>{isResult}</Text>
       </View>
       <View style={styles.content}>
         <View style={styles.leftBtn}>
-          <View style={styles.item}>
-            {leftItems.map((item, i) => {
-              return (
-                <TouchableOpacity
-                  key={i}
-                  onPress={buttonClickedHandler}
-                  style={styles.roundButton1}
-                >
-                  <Text>{item}</Text>
-                </TouchableOpacity>
-              );
-            })}
-          </View>
+          {leftItems.map((item, i) => {
+            return (
+              <TouchableOpacity
+                key={i}
+                onPress={() => handleClick(item)}
+                style={styles.roundButton1}
+              >
+                <Text>{item}</Text>
+              </TouchableOpacity>
+            );
+          })}
         </View>
+
         <View style={styles.rightBtn}>
-          <View style={styles.item}>
-            {rightItems.map((item, i) => {
-              return (
-                <TouchableOpacity
-                  key={i}
-                  onPress={buttonClickedHandler}
-                  style={styles.roundButton1}
-                >
-                  <Text style={{ fontSize: 30 }}>{item}</Text>
-                </TouchableOpacity>
-              );
-            })}
-          </View>
+          {rightItems.map((item, i) => {
+            return (
+              <TouchableOpacity
+                key={i}
+                onPress={() => handleClick(item)}
+                style={styles.roundButton1}
+              >
+                <Text style={{ fontSize: 30 }}>{item}</Text>
+              </TouchableOpacity>
+            );
+          })}
+
           <View>
             <TouchableOpacity
-              onPress={buttonClickedHandler}
+              onPress={() => handleClickEq()}
               style={styles.btnEq}
             >
               <Text style={{ fontSize: 50, color: "white" }}>=</Text>
@@ -79,18 +88,20 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
     flexDirection: "column",
+    backgroundColor: "coral",
   },
   header: {
     flex: 2,
     alignItems: "flex-end",
     paddingHorizontal: 20,
     justifyContent: "center",
-    backgroundColor: "lightgray",
+    backgroundColor: "white",
   },
   btnEq: {
-    margin: 10,
+    marginTop: 15,
+    margin: 6,
     width: 65,
-    height: 120,
+    height: 154,
     backgroundColor: `#f08080`,
     borderRadius: 50,
     justifyContent: "flex-end",
@@ -98,20 +109,22 @@ const styles = StyleSheet.create({
   },
   leftBtn: {
     flex: 3,
-    backgroundColor: "lightblue",
+    backgroundColor: "white",
     flexDirection: "row-reverse",
+
     flexWrap: "wrap",
   },
   rightBtn: {
     flex: 1,
 
-    backgroundColor: "gray",
+    backgroundColor: "white",
   },
 
   roundButton1: {
-    margin: 10,
-    width: 65,
-    height: 65,
+    marginTop: 15,
+    margin: 6,
+    width: 67,
+    height: 67,
     justifyContent: "center",
     alignItems: "center",
     padding: 10,
@@ -119,8 +132,8 @@ const styles = StyleSheet.create({
     backgroundColor: `#d3d3d3`,
   },
   content: {
-    flex: 5,
-    backgroundColor: "coral",
+    flex: 4,
+    borderTopEndRadius: 20,
     flexDirection: "row",
   },
 });
